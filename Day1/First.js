eec6bd9c262003e633730f0ddc1d4fc3132b0edd
Dipul Raj <krishnasharma405345@gmail.com>
2026-03-23 20:22:02 +0530

    1st lecture completed

--- Day1/First.js
+++ Day1/First.js
@@ -1,16 +1,21 @@
 //Creating Nested Elements
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { id: "heading1" }, "Hi i am Nested Heading1"),
     React.createElement("h1", { id: "heading2" }, "Hi i am Nested Heading2"),
   ]),
-);
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading1" }, "Hi i am Nested Heading1"),
+    React.createElement("h1", { id: "heading2" }, "Hi i am Nested Heading2"),
+  ]),
+]);
 
 // const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
 
 const root = ReactDOM.createRoot(document.getElementById("box"));
+const root1 = ReactDOM.createRoot(document.getElementById("box1"));
+
 
 root.render(parent);
+root1.render(parent);
