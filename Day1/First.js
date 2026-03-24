e4dcff3f3527e1e75dc6ed92f3a47f0b141d88ac
Dipul Raj <krishnasharma405345@gmail.com>
2026-03-25 00:23:03 +0530

    Level 3rd completed

--- Day1/First.js
+++ Day1/First.js
@@ -1,21 +1,41 @@
 //Creating Nested Elements
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading1" }, "Hi i am Nested Heading1"),
-    React.createElement("h1", { id: "heading2" }, "Hi i am Nested Heading2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading1" }, "Hi i am Nested Heading1"),
-    React.createElement("h1", { id: "heading2" }, "Hi i am Nested Heading2"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1", key: "ch1"}, [
+//     React.createElement("h1", { id: "heading1", key: "h1"}, "Hi i am Nested Heading1"),
+//     React.createElement("h1", { id: "heading2", key: "h2"}, "Hi i am Nested Heading2"),
+//   ]),
+//   React.createElement("div", { id: "child2", key: "ch2"}, [
+//     React.createElement("h1", { id: "heading3", key: "h3"}, "Hi i am Nested Heading1"),
+//     React.createElement("h1", { id: "heading4", key: "h4"}, "Hi i am Nested Heading2"),
+//   ]),
+// ]);
 
 // const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
 
+let Title = () => {
+  return <h1>Hi i am Title</h1>;
+};
+
+// let title = () => {
+//   <h1>Hi i am Title</h1>;
+// };
+
+
+let Heading = () => {
+  return (
+    <div id="container">
+      <Title />
+      {Title()}
+      <h1 id="heading">Hello world Using JSX</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("box"));
-const root1 = ReactDOM.createRoot(document.getElementById("box1"));
+// const root1 = ReactDOM.createRoot(document.getElementById("box1"));
 
-
-root.render(parent);
-root1.render(parent);
+root.render(<Heading />);
+// root1.render(parent);
